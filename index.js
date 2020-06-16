0c3a63434ebdc7dfcb0ffbc29d1a69bc92811546
Ray Deck <ray@raydeck.com>
2020-06-16 05:21:20 -0500

    Adding new functionality to only show appropriate apps for a platform, update stages across apps, and make deploying to a given stage much smoother

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const {
   android: { setString },
   ios: { setPlistValue },
 } = require("@raydeck/react-native-utilities");
+const { readFileSync, writeFileSync } = require("fs");
 const getCommitHash = () =>
   execSync("git log -1").toString().split("\n").shift();
 const release = async ({
@@ -15,118 +16,138 @@ const release = async ({
   isMandatory = false,
 }) => {
   if (!targetVersion)
-    targetVerison = require(join(process.cwd(), "package.json")).version;
+    targetVersion = require(join(process.cwd(), "package.json")).version;
   const descriptionOut = [description, addGithash && getCommitHash()]
     .filter(Boolean)
     .join("\n\n");
-  let cmd = `appcenter codepush release-react -a ${appName} -d ${stage} -t ${version} --description '${descriptionOut}'`;
+  let cmd = `yarn -s appcenter codepush release-react -a ${appName} -d ${stage} -t ${targetVersion} --description '${descriptionOut}'`;
   if (isMandatory) cmd += ` -m`;
+  console.log("running ", cmd);
   execSync(cmd, { stdio: "inherit" });
 };
-
-const setKey = async ({
+const getKey = async ({
   appName,
   stage,
   android,
   ios,
-  key,
   path = process.cwd(),
 }) => {
   if (!android && !ios) {
     throw "Must specify android or ios for setting the key";
   }
-  if (!key) {
-    // if (!stage || !appName)
-    //   throw "Either key or [appname, stage] is required to set the key";
-    if (!key) {
-      if (!appName) {
-        //List appnames
-        const { output } = spawnSync(
-          "yarn",
-          ["-s", "appcenter", "apps", "list", "--output", "json"],
-          { stdio: "pipe", encoding: "utf8" }
-        );
-        const a = JSON.parse([output[0], output[1]].filter(Boolean).join(""));
-        if (a.length > 1) {
-          console.log(
-            "Problem: appName not specified - try running with one of these names"
-          );
-          console.log(
-            a
-              .map(
-                ({ name, owner: { name: ownerName } }) =>
-                  "\t react-native set-code-push-key " +
-                  (ios ? " --ios" : "") +
-                  (android ? " --android" : "") +
-                  " --appName " +
-                  [ownerName, name].join("/")
-              )
-              .join("\n")
-          );
-          return;
-        } else {
-          const {
-            name,
-            owner: { name: ownerName },
-          } = a.pop();
-          appName = [ownerName, name].join("/");
-        }
-      }
-      const { output: output2 } = spawnSync(
-        "yarn",
-        [
-          "-s",
-          "appcenter",
-          "codepush",
-          "deployment",
-          "list",
-          "-k",
-          "--app",
-          appName,
-          "--output",
-          "json",
-        ],
-        { stdio: "pipe", encoding: "utf8" }
+  let key;
+  if (!appName && !stage) {
+    //List appnames
+    const { output } = spawnSync(
+      "npx",
+      ["appcenter", "apps", "list", "--output", "json"],
+      { stdio: "pipe", encoding: "utf8" }
+    );
+    const a = JSON.parse([output[0], output[1]].filter(Boolean).join(""));
+    if (a.length > 1) {
+      console.log(
+        "Problem: appName not specified - try running with one of these names"
       );
-      if (!output2) {
-        throw "Could not find codepush stages for app " + appName;
-      }
-      const a2 = JSON.parse([output2[0], output2[1]].filter(Boolean).join(""));
-      if (stage) {
-        const myStage = a2.find(([thisStage, key]) => thisStage === stage);
-        if (myStage) {
-          key = myStage[1];
-        } else {
-          throw "This stage does not exist in the app " + appName;
-        }
-      } else if (a2.length > 1) {
-        console.log(
-          "Problem: stage not specified, and there is more than one. Try using:"
-        );
-        console.log(
-          a2
-            .map(
-              ([stage]) =>
-                "\treact-native set-code-push-key --appName " +
-                appName +
-                " --stage " +
-                stage +
-                (ios ? " --ios" : "") +
-                (android ? " --android" : "")
-            )
-            .join("\n")
-        );
-        return;
-      } else {
-        const [[, newKey]] = stages;
-        key = newKey;
-      }
+      console.log(
+        a
+          .filter(
+            ({ os }) => (ios && os === "iOS") || (android && os === "Android")
+          )
+          .map(
+            ({ name, owner: { name: ownerName } }) =>
+              "\t react-native set-code-push-key " +
+              (ios ? " --ios" : "") +
+              (android ? " --android" : "") +
+              " --appName " +
+              [ownerName, name].join("/")
+          )
+          .join("\n")
+      );
+      return;
+    } else {
+      const {
+        name,
+        owner: { name: ownerName },
+      } = a.pop();
+      appName = [ownerName, name].join("/");
     }
-    if (key) {
-      if (android) await setString("CodePushDeploymentKey", key, path);
-      if (ios) setPlistValue("CodePushDeploymentKey", key, path);
-    }
-    return stage || true;
+  } else {
+    const o = JSON.parse(readFileSync(join(path, "package.json")));
+    const { codepush } = o;
+    if (ios) appName = codepush.ios.appName;
+    if (android) appName = codepush.android.appName;
   }
+  const { output: output2 } = spawnSync(
+    "npx",
+    [
+      "appcenter",
+      "codepush",
+      "deployment",
+      "list",
+      "-k",
+      "--app",
+      appName,
+      "--output",
+      "json",
+    ],
+    { stdio: "pipe", encoding: "utf8" }
+  );
+  if (!output2) {
+    throw "Could not find codepush stages for app " + appName;
+  }
+  const a2 = JSON.parse([output2[0], output2[1]].filter(Boolean).join(""));
+  if (stage) {
+    const myStage = a2.find(([thisStage, key]) => thisStage === stage);
+    if (myStage) {
+      key = myStage[1];
+    } else {
+      throw "This stage does not exist in the app " + appName;
+    }
+  } else if (a2.length > 1) {
+    console.log(
+      "Problem: stage not specified, and there is more than one. Try using:"
+    );
+    console.log(
+      a2
+        .map(
+          ([stage]) =>
+            "\treact-native set-code-push-key --appName " +
+            appName +
+            " --stage " +
+            stage +
+            (ios ? " --ios" : "") +
+            (android ? " --android" : "")
+        )
+        .join("\n")
+    );
+    return;
+  } else {
+    const [[, newKey]] = stages;
+    key = newKey;
+  }
+  return { appName, key, stage, ios, android };
 };
-module.exports = { setKey, release };
+const saveToBinary = async ({ android, ios, key, path = process.cwd() }) => {
+  if (android) await setString("CodePushDeploymentKey", key, path);
+  if (ios) setPlistValue("CodePushDeploymentKey", key, path);
+};
+const saveToPackage = ({
+  android,
+  ios,
+  stage,
+  appName,
+  path = process.cwd(),
+}) => {
+  const packagePath = join(path, "package.json");
+  let o = JSON.parse(readFileSync(packagePath, { encoding: "utf8" }));
+  let codepush = o.codepush || {};
+  if (android) {
+    codepush.android = { stage, appName };
+  }
+  if (ios) {
+    codepush.ios = { stage, appName };
+  }
+  o.codepush = codepush;
+  writeFileSync(packagePath, JSON.stringify(o, null, 2));
+};
+module.exports = { getKey, saveToBinary, release, saveToPackage };
--- react-native.config.js
+++ react-native.config.js
@@ -1,6 +1,6 @@
-const { release, setKey } = require("./");
+const { release, getKey, saveToBinary, saveToPackage } = require("./");
 const { join } = require("path");
-const { readFileSync, writeFileSync } = require("fs");
+const { readFileSync } = require("fs");
 const packagePath = join(process.cwd(), "package.json");
 const releaseIos = async ({
   targetVersion,
@@ -8,23 +8,28 @@ const releaseIos = async ({
   description,
   addGithash,
 }) => {
+  console.log("starting releaseios");
+  let appName, stage;
   try {
-    const {
-      codepush: {
-        ios: { appName, stage },
-      },
+    var {
+      codepush: { ios },
     } = JSON.parse(readFileSync(packagePath, { encoding: "utf8" }));
-    return release({
-      appName,
-      stage,
-      targetVersion,
-      isMandatory,
-      description,
-      addGithash,
-    });
+    appName = ios.appName;
+    stage = ios.stage;
   } catch (e) {
-    ("There is no saved ios setting in package.json - this is created when you set the key through 'react-native set-code-push-key'");
+    console.error(
+      "There is no saved ios setting in package.json - this is created when you set the key through 'react-native set-code-push-key'"
+    );
   }
+  console.log("we will try to get to release");
+  return release({
+    appName,
+    stage,
+    targetVersion,
+    isMandatory,
+    description,
+    addGithash,
+  });
 };
 const releaseAndroid = async ({
   targetVersion,
@@ -32,25 +37,26 @@ const releaseAndroid = async ({
   description,
   addGithash,
 }) => {
+  let appName, stage;
   try {
-    const {
-      codepush: {
-        ios: { appName, stage },
-      },
+    var {
+      codepush: { ios },
     } = JSON.parse(readFileSync(packagePath, { encoding: "utf8" }));
-    return release({
-      appName,
-      stage,
-      targetVersion,
-      isMandatory,
-      description,
-      addGithash,
-    });
+    appName = ios.appName;
+    stage = ios.stage;
   } catch (e) {
     console.error(
       "There is no saved android setting in package.json - this is created when you set the key through 'react-native set-code-push-key'"
     );
   }
+  return release({
+    appName,
+    stage,
+    targetVersion,
+    isMandatory,
+    description,
+    addGithash,
+  });
 };
 module.exports = {
   commands: [
@@ -110,6 +116,7 @@ module.exports = {
           mandatory: isMandatory,
         }
       ) => {
+        console.log("starting release");
         if (appName && (ios || android)) {
           console.error(
             "--ios and --android and substitutes for selecting the appname - you cannot do both"
@@ -118,20 +125,52 @@ module.exports = {
         }
         try {
           if (!appName) {
-            if (ios)
-              await releaseIos({
-                description,
-                addGithash,
-                targetVersion,
-                isMandatory,
-              });
-            if (android)
-              await releaseAndroid({
-                description,
-                addGithash,
-                targetVersion,
-                isMandatory,
-              });
+            if (stage) {
+              if (ios) {
+                const { key, appName } = await getKey({ ios, stage });
+                saveToBinary({ ios, key });
+                saveToPackage({ ios, stage, appName });
+              }
+              if (android) {
+                const { key, appName } = await getKey({ android, stage });
+                saveToBinary({ android, key });
+                saveToPackage({ android, stage, appName });
+              }
+            }
+            if (!ios && !android) {
+              console.error("Must specify --ios and/or --android");
+              process.exit(1);
+            }
+            const errors = [];
+            if (ios) {
+              try {
+                await releaseIos({
+                  description,
+                  addGithash,
+                  targetVersion,
+                  isMandatory,
+                });
+              } catch (e) {
+                errors.push(e);
+              }
+            }
+            if (android) {
+              try {
+                await releaseAndroid({
+                  description,
+                  addGithash,
+                  targetVersion,
+                  isMandatory,
+                });
+              } catch (e) {
+                errors.push(e);
+              }
+            }
+            if (errors.length) {
+              console.error(
+                errors.length + " processes failed. Exiting with error signal"
+              );
+            }
           } else
             release({
               appName,
@@ -148,7 +187,7 @@ module.exports = {
       },
     },
     {
-      name: "set-code-push-key [key]",
+      name: "set-code-push-key ",
       description: "Set codepush key (platform specification required)",
       options: [
         {
@@ -170,36 +209,56 @@ module.exports = {
           default: "",
         },
       ],
-      func: async (
-        [key],
-        _,
-        { appName, stage, android, ios, doSave = true }
-      ) => {
+      func: async (__, _, { appName, stage, android, ios, doSave = true }) => {
         try {
-          const tempStage = await setKey({ appName, stage, android, ios, key });
-          if (doSave && tempStage) {
-            const thisStage = stage || tempStage;
-            if (appName && typeof stage === "string") {
-              //Save this to the string
-
-              let o = JSON.parse(
-                readFileSync(packagePath, { encoding: "utf8" })
-              );
-              let codepush = o.codepush || {};
-              if (android) {
-                codepush.android = { stage: thisStage, appName };
-              }
-              if (ios) {
-                codepush.ios = { stage: thisStage, appName };
-              }
-              writeFileSync(packagePath, JSON.stringify(o, null, 2));
-            }
-          }
+          if (!ios && !android) console.error("Must choose --ios or --android");
+          if (ios && android)
+            console.error("--ios and --android options are mutually exclusive");
+          const { key } = await getKey({ appName, stage, android, ios });
+          saveToBinary({ android, ios, key, stage, android });
+          saveToPackage({ android, ios, stage, appName });
           console.log("Success!");
         } catch (e) {
-          console.error(e);
+          // console.error(e);
           process.exit(1);
         }
+      },
+    },
+    {
+      name: "code-push-set-stage [stage]",
+      description:
+        "Update all codepush tokens to the same stage of their respective apps (e.g. test-3 or the like)",
+      options: [
+        {
+          name: "--ios",
+          description: "update IOS deployment",
+        },
+        {
+          name: "--android",
+          description: "update Android deployment",
+        },
+      ],
+      func: async ([stage], __, { ios, android }) => {
+        if (ios) {
+          const { key, appName } = await getKey({ ios, stage });
+          saveToBinary({ ios, key });
+          saveToPackage({ ios, stage, appName });
+        }
+        if (android) {
+          const { key, appName } = await getKey({ android, stage });
+          saveToBinary({ android, key });
+          saveToPackage({ android, stage, appName });
+        }
+      },
+    },
+    {
+      name: "code-push-list",
+      description: "List code push apps and stages attached to this app",
+      func: () => {
+        const { codepush } = JSON.parse(
+          readFileSync(join(process.cwd(), "package.json"))
+        );
+        console.log(JSON.stringify(codepush, null, 2));
       },
     },
   ],
